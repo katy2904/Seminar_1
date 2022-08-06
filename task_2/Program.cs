7d001596e21e65ed455f690a7a7b1dbbd56a6b2b
Katy <wc-katy@yandex.ru>
2022-08-06 22:01:15 +0700

    Задача 2 коррекция

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -9,11 +9,5 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > number2)
-{
-    Console.WriteLine("max = " + number1 + ", min = " + number2);
-}
-else
-{
-    Console.WriteLine("max = " + number2 + ", min = " + number1);
-}
+if (number1 > number2) Console.WriteLine("max = " + number1 + ", min = " + number2);
+else Console.WriteLine("max = " + number2 + ", min = " + number1);
