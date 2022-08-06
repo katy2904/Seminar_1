5438ea6f9d5da051d59388262563ae38f0ae7763
Katy <wc-katy@yandex.ru>
2022-08-06 22:37:59 +0700

    Задача 8 из ДЗ к семинару 1

---
+++ task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.
+*/
+
+Console.Write("Введите число: "); 
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count += 2;
+}
